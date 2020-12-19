fc4a8dd34a2937df06155663670efe4a6fa33464
Dominik <dom.runge@campus.lmu.de>
2020-12-19 18:05:02 +0100

    Newest changes

--- a-05/MemeServer/models/meme.js
+++ a-05/MemeServer/models/meme.js
@@ -2,13 +2,24 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 // This is a schema, only one file per schema! 
-const MemeSchema = new Schema(
+/*const MemeSchema = new Schema(
   {
     code: Number,
+    url: String,
     title: String,
-    artist: String,
-    album: String,
+    title2: String,
+    title3: String,
+    voting: Number,
+    comments: Number,
     runtime: Number
- });
+ });*/
+ const MemeSchema = new Schema(
+    {
+        memeURLs: {           
+            'type': {type: String},
+            'value': [String]
+            },
+    }); 
+
 
 module.exports = mongoose.model('meme', MemeSchema );
--- a-05/MemeServer/routes/memes.js
+++ a-05/MemeServer/routes/memes.js
@@ -1,40 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
-// let memes = {memeURLs: [ //sample memes
-//     'https://i.imgflip.com/4pke9r.jpg',
-//     'https://i.imgflip.com/4osa73.jpg',
-//     'https://i.imgflip.com/4pcn16.jpg'
-// ]};
-
-router.get('/', function (req, res) {
-    // const db = req.db;
-
-    let memes = db.get('Memes');
-    // memes.find({}, { url: 1 }).toArray(function(err, docs){
-    //     console.log("retrieved records:");
-    //     console.log(docs);
-    //     return docs;
-    // });
-    memes.find({}, { url: 1 })
-        .then((docs) => res.json(docs))
-        .catch((e) => res.status(500).send())
+//Mongo
+const Meme = require('../models/meme.js');
 
 
-    res.send({
-        code: 200,
-        memeURLs: memes
-    })
+//Mongo 
+router.get('/', function(req, res, next) {
+  Meme.find({},function(err, list){ if (err) {
+    console.log(err); console.log("hihi");}
+  else {
+    console.log(list);
+    console.log("hoho");
+    res.render('memes', {memelist: list}); }
+  }) 
+});
 
-})
+
+// /* GET meme URLs. */
+// router.get('/', function(req, res, next) {
+//     res.send({
+//         code: 200,
+//         memes
+//       });
+//   });
 
 /* GET meme URLs. */
-// router.get('/', function(req, res, next) {
-//   res.send({
-//       code: 200,
-//       memes
-//     });
-// });
+ router.get('/', function(req, res, next) {
+   res.send({
+       code: 200,
+       memes
+     });
+ });
 
 /* POST a meme URL */
 router.post('/', function(req, res, next) {
