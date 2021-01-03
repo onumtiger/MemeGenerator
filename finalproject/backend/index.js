a10bb39d87a17d2603979a319633b5b3742cd5b6
onum <>
2021-01-03 16:16:28 +0100

    added backend

--- finalproject/backend/index.js
+++ finalproject/backend/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const db = require('./db/index.js')
+const memeRouter = require('./routes/meme-router.js')
 const app = express()
 
 const PORT = 3000
@@ -9,8 +11,12 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors())
 app.use(bodyParser.json())
 
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!!')
 })
+
+app.use('/api', memeRouter)
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
