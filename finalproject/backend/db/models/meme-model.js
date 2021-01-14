86cf8439a2f73ff6d6171c591e8046975a2d3c75
Dominik <dom.runge@campus.lmu.de>
2021-01-15 00:05:02 +0100

    Database reset integration added

--- finalproject/backend/db/models/meme-model.js
+++ finalproject/backend/db/models/meme-model.js
@@ -4,15 +4,18 @@ const Schema = mongoose.Schema
 const MemeSchema = new Schema(
   {
     id: { type: Number, required: true},
+    url: { type: String, required: true},
+    captions: { type: [String], required: true },
     box_count: { type: Number, required: false},
     height: { type: Number, required: false},
     width: { type: Number, required: false},
-    url: { type: String, required: true},
     name: { type: String, required: false},
-    captions: { type: [String], required: true },
     runtime: { type: Number, required: false},
+    comments: { type: [String], required: false },
+    votes: { type: Number, required: false },
+    views: { type: Number, required: false},
+    creationDate: { type: String, required: false}
  }); 
-//comments, voting, stats tbd
 
 
 module.exports = mongoose.model('Meme', MemeSchema );
