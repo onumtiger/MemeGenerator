baec6de599506c1f6b1e0ff0121e8eca20701c20
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2021-01-16 18:56:10 +0100

    2 small fixes

--- finalproject/frontend/src/components/CanvasDownloadButton.jsx
+++ finalproject/frontend/src/components/CanvasDownloadButton.jsx
@@ -2,10 +2,6 @@ import React from 'react';
 import '../style/CanvasDownloadButton.scss';
 
 export default class CanvasDownloadButton extends React.Component {
-    constructor(props){
-        super(props);
-    }
-
     render(){
         return (
             <a id="download-anchor" download={this.props.placeholderFileName}>
