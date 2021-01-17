defd8efb01d3c762b9853dc17920528f0446a220
Chau Duong <c.duong@campus.lmu.de>
2021-01-17 18:45:37 +0100

    upload frontend

--- finalproject/frontend/src/pages/Upload.jsx
+++ finalproject/frontend/src/pages/Upload.jsx
@@ -9,11 +9,12 @@ class UploadMeme extends React.Component {
         super(props);
         this.state = {
             image: null,
-            // dragging: false
+            id: '',
+            name: ''
         };
 
         this.onImageChange = this.onImageChange.bind(this);
-        // this.submitFile = this.submitFile.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
     onImageChange = event => {
@@ -22,22 +23,60 @@ class UploadMeme extends React.Component {
             this.setState({
                 image: URL.createObjectURL(img)
             });
-            console.log(this.state.image )
+            console.log(this.state.image)
         }
+    }
+
+    getUploadConfig() {
+        const container = document.getElementById('uploadContainer');
+        const id = container.querySelector('#id').value;
+        const name = container.querySelector('#name').value;
+        return { id: id, name: name }
+    }
+
+    handleSubmit() {
+        const container = document.getElementById('uploadContainer');
+        const file = container.querySelector('input[type="file"]').files[0];
+
+        const { id, name } = this.getUploadConfig();
+
+        const formData = new FormData();
+        formData.append('image', file);
+        formData.append('name', name)
+
+        // --- the REQUEST which is used for FILE UPLOAD ---
+        fetch(`http://localhost:3000/images/${id}`, {
+            method: 'POST',
+            mode: 'cors',
+            body: formData
+        }).then(res => {
+            console.log(res);
+        })
     }
 
     render() {
         return (
             <div className="wrapper">
-                <h2>Upload a meme and add it to your template</h2>
-                <label>Upload your own meme: </label>
-
-                <div id="drop-area">
-                    <div><img id="output" src={this.state.image} /></div>
-                    <input type="file" id="fileElem" multiple accept="image/*" onChange={this.onImageChange} />
-                    <label className="button" htmlFor="fileElem">Choose a meme</label>
-                </div>
-                <button id="uploadButtonFile" type="submit" onClick={this.submitFile}>Upload File</button>
+                <h2>Upload a meme and add it to your templates</h2>
+                <form id="uploadContainer" onSubmit={this.handleSubmit} encType='multipart/form-data'>
+                    <label>Upload your own meme: </label>
+                    <div id="drop-area">
+                        <div><img id="output" src={this.state.image} /></div>
+                        <input type="file" id="fileElem" multiple accept="image/*" onChange={this.onImageChange} />
+                        <label className="button" htmlFor="fileElem">Choose a meme</label>
+                    </div>
+                    <table>
+                        <tr>
+                            <td><label for="id">ID</label></td>
+                            <td><input id="id" type="text" /></td>
+                        </tr>
+                        <tr>
+                            <td><label for="name">Name</label></td>
+                            <td><input id="name" type="text" /></td>
+                        </tr>
+                    </table>
+                    <button id="uploadButtonFile" type="submit">Upload File</button>
+                </form>
 
                 <div>
                     <label>Template memes:</label>
