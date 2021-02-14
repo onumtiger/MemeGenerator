987e57cfe519512865af251b9b8cc2bcd6f735f7
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2021-02-14 20:48:51 +0100

    finalized templateschema

--- finalproject/backend/db/models/template-model.js
+++ finalproject/backend/db/models/template-model.js
@@ -3,13 +3,13 @@ const Schema = mongoose.Schema
 
 const TemplateSchema = new Schema(
   {
-    id: { type: Number, required: true}, //template ID
+    _id: { type: Number, required: true}, //template ID
     url: { type: String, required: true}, //template image URL
     name: { type: String, required: true}, //template name / short description
-    uploader: { type: String, required: true}, //uploader of this template - TODO reference user DB instance by ID or similar
+    uploader_id: { type: Number, required: true}, //uploader of this template - reference user DB instance by ID or similar
     visibility: { type: Number, required: true}, //visibility of this template: 0 (private) or 1 (public) [2 (unlisted) does not make much sense for templates ]
-    numUsed: { type: Number, required: false} //number of uses of this templates to create a meme - TODO replace with reference to (Template)Stats DB instance by ID
- });
+    stats_id: { type: Number, required: false} //reference to (Template)Stats DB instance by ID
+ }, { collection: 'templates' });
 
 
 module.exports = mongoose.model('template', TemplateSchema );
