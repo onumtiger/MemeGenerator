10f9bbcac245a45ae068d1a4f183e3c33eeced83
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2021-02-14 21:22:41 +0100

    updated template schema for consistency

--- finalproject/backend/db/models/template-model.js
+++ finalproject/backend/db/models/template-model.js
@@ -6,9 +6,9 @@ const TemplateSchema = new Schema(
     _id: { type: Number, required: true}, //template ID
     url: { type: String, required: true}, //template image URL
     name: { type: String, required: true}, //template name / short description
-    uploader_id: { type: Number, required: true}, //uploader of this template - reference user DB instance by ID or similar
+    user_id: { type: Number, required: true}, //uploader of this template - reference user DB instance by ID or similar
     visibility: { type: Number, required: true}, //visibility of this template: 0 (private) or 1 (public) [2 (unlisted) does not make much sense for templates ]
-    stats_id: { type: Number, required: false} //reference to (Template)Stats DB instance by ID
+    stats_id: { type: Number, required: true} //reference to (Template)Stats DB instance by ID
  }, { collection: 'templates' });
 
 
