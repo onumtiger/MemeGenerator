6e6935201cdbc464959fbdb45197858fea81654d
Dominik <dom.runge@campus.lmu.de>
2021-02-17 22:51:36 +0100

    Added memes to database

--- finalproject/backend/db/index.js
+++ finalproject/backend/db/index.js
@@ -17,9 +17,9 @@ db.collection('memes').deleteMany({}).then(function(){
 
 var defaultMeme = {
     _id: 0,
-    url: 'testURL',
-    captions: ['testCaption1', 'testCaption2'], 
-    name: 'memeName', 
+    url: '/memes/jan_domi_zusammentreffen',
+    captions: ['Domi', 'Jan Jbernolte'], 
+    name: 'Jan und Domi unterwegs', 
     stats_id: 0, 
     comment_ids: [0, 1],
     user_id: 0, 
@@ -29,9 +29,9 @@ var defaultMeme = {
 
 var defaultMeme2 = {
     _id: 1,
-    url: 'testURL',
-    captions: ['testCaption1', 'testCaption2'], 
-    name: 'memeName', 
+    url: '/memes/jan_domi_punch',
+    captions: ['Domi', 'Jan Obernolte'], 
+    name: 'Jan und Domis Gefühle füreinander <3', 
     stats_id: 1, 
     comment_ids: [2, 3],
     user_id: 1, 
@@ -41,12 +41,12 @@ var defaultMeme2 = {
 
 var defaultMeme3 = {
     _id: 2,
-    url: 'testURL',
-    captions: ['testCaption1', 'testCaption2'], 
-    name: 'memeName', 
+    url: '/memes/jan_domi_cat',
+    captions: ['Dominik', 'Jan Obernolte'], 
+    name: 'Jan als Katze', 
     stats_id: 2, 
     comment_ids: [3, 4],
-    user_id: 0, 
+    user_id: 2, 
     visibilty: 2,
     creationDate: '12021997'
 };
@@ -56,6 +56,7 @@ db.collection('memes').insertMany([defaultMeme, defaultMeme2, defaultMeme3]).the
 
 
 // mongoose lokal db USER reset, without SCHMEMA
+
 db.collection('users').deleteMany({}).then(function(){ 
     console.log("Old users deleted") });
 
