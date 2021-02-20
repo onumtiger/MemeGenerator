1b1529a692ea6d5d44639de8f0cbdea662a9e073
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2021-02-20 04:33:51 +0100

    getTodayString() as yyyy/mm/dd for better sorting

--- finalproject/backend/utils/globalHelpers.js
+++ finalproject/backend/utils/globalHelpers.js
@@ -8,7 +8,7 @@ module.exports = {
         let day = `${d.getDate()}`.padStart(2, '0');
         let month = `${d.getMonth()+1}`.padStart(2, '0');
         let year = d.getFullYear();
-        return `${day}/${month}/${year}`; //form: dd/mm/yyyy
+        return `${year}/${month}/${day}`; //form: yyyy/mm/dd
     },
     getNewEmptyTemplateID: async ()=>{
         let prevTemplates = await Template.find({});
