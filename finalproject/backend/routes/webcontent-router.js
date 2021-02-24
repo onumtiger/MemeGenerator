6b2b14812937d3685b7f867e2c9f68d7b6c798c0
Jan Obernolte <obernolte@cip.ifi.lmu.de>
2021-02-24 03:48:16 +0100

    screenshot templates: - implemented screenshot templates via newly installed puppeteer package - added comments to webcontent-ctrl.js

--- finalproject/backend/routes/webcontent-router.js
+++ finalproject/backend/routes/webcontent-router.js
@@ -3,7 +3,9 @@ const WebContentCtrl = require('../controllers/webcontent-ctrl');
 
 const router = express.Router();
 
-router.get('/', WebContentCtrl.test);
+//for debugging URL query Strings:
+router.get('/queryTest', WebContentCtrl.testQueryParams);
+
 router.get('/image/:url', WebContentCtrl.getWebImage);
 router.get('/snapshot/:url', WebContentCtrl.getWebSnapshot);
 
