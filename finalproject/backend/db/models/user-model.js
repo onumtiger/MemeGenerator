b6d6341e97df134a95e22725e3f47941c404f002
onum <>
2021-02-27 17:53:38 +0100

    updated user model

--- finalproject/backend/db/models/user-model.js
+++ finalproject/backend/db/models/user-model.js
@@ -3,9 +3,23 @@ const Schema = mongoose.Schema
 
 const UserSchema = new Schema(
   {
-    _id: { type: Number, required: true}, // individual user id (not displayed)
-    username: { type: String, required: true}, // displayed username 
+    // _id: { type: Number, required: true}, // individual user id (not displayed)
+    // id: { type: Number, required: true}, // individual user id (not displayed)
+    _id: {type: mongoose.Schema.Types.ObjectId},
+    email: {
+      type: String, 
+      required: true, 
+      unique: true,
+      match: /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9]))\.){3}(?:(2(5[0-5]|[0-4][0-9])|1[0-9][0-9]|[1-9]?[0-9])|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    },
+    username: { 
+      type: String, 
+      required: true, 
+      unique: true,
+      // match: /^(?=[a-zA-Z0-9._]{8,20}$)(?!.*[_.]{2})[^_.].*[^_.]$/
+    }, // displayed username 
     password: { type: [String], required: true }, // password for login
+    meme_ids: { type: [Number], required: false} // user created memes (Array)
  }, { collection: 'users' }); 
 
 
