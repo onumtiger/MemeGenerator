5596b9c01b22e5a62a363c1ff38d1d5bf1ef2738
Chau Duong <c.duong@campus.lmu.de>
2021-02-27 20:57:20 +0100

    comments posted in real time

--- finalproject/frontend/src/components/MemeComment.jsx
+++ finalproject/frontend/src/components/MemeComment.jsx
@@ -6,64 +6,23 @@ export default class Comment extends Component {
 
     constructor(props) {
         super(props);
-        
-
-        this.state = {
-            comments: this.props.comments,
-            dates: this.props.dates,
-            userId: this.props.userId
-        }
-    }
-
-    getTodayString= ()=>{
-        let d = new Date();
-        let day = `${d.getDate()}`.padStart(2, '0');
-        let month = `${d.getMonth()+1}`.padStart(2, '0');
-        let year = d.getFullYear();
-        return `${year}/${month}/${day}`; //form: yyyy/mm/dd
     }
 
     handlePost = async () => {
         const input = document.getElementById('commentInput').value;
-        
-        var comments = this.state.comments
-        comments.push(input);
-
-        var dates = this.state.dates
-        dates.push(this.getTodayString())
-
-        
-
-        this.setState({
-            comments: comments
-        })
-
-        // let refreshedComments = this.props.comments
-        // refreshedComments.push(input)
-        // this.state.comments = refreshedComments;
 
         // TODO change when login feature merged
-        let user_id=0;
-        let comment=input
-        let meme_id = this.props.id
+        let user_id = 0;
+        let meme_id = this.props.id;
+        let message = input;
 
-        console.log("user id", user_id);
-        console.log("comment", comment)
-        console.log("meme_id", meme_id)
-
-        await api.postComment(user_id, meme_id, comment).catch(err =>{
-            console.log('Failed to send comment: ',err);
+        await api.postComment(user_id, meme_id, message).catch(err => {
+            console.log('Failed to send comment: ', err);
         });
 
+        this.props.getComments();
+
         document.getElementById('commentInput').value = '';
-    }
-
-    removeComment = (targetIndex) => {
-        // setComments((prev) => {
-        //     return prev.filter((comment, index) => index !== targetIndex);
-        // });
-
-        // setCommentCounter(prevCount => prevCount - 1);
     }
 
     handleKeypress = e => {
@@ -86,7 +45,7 @@ export default class Comment extends Component {
             <div>
                 <p className="commentNumber">{this.props.commentCount} comments</p>
                 <div>
-                    {this.state.comments.map((comment, index) => (
+                    {this.props.comments.map((comment, index) => (
                         <div key={index}>
                             <div className="commentInfo">
                                 <div className="commenDate">
@@ -95,7 +54,6 @@ export default class Comment extends Component {
                                 <div className="userInfo"><label className="username">User_{this.props.userId[index]}</label>:</div>
                             </div>
                             <label className="commentText">{comment}</label>
-                            {/* <div className="deleteIcon" onClick={() => removeComment(index)}>&times;</div> */}
                         </div>
                     ))}
                 </div>
